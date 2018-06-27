296b486909c0e0b6276494c8c9931b70b0631bfc
601687170 <601687170@qq.com>
2018-06-27 15:36:06 +0800

    Example5 V1.1

--- SpringBootDemo/src/main/java/com/cave/springbootdemo/example5/config/RedisConfig.java
+++ SpringBootDemo/src/main/java/com/cave/springbootdemo/example5/config/RedisConfig.java
@@ -1,6 +1,15 @@
 package com.cave.springbootdemo.example5.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * Redis技术相关博客：https://segmentfault.com/a/1190000012038561
@@ -10,6 +19,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RedisConfig {
 
+    @Bean
+    public RedisTemplate getRedisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(connectionFactory);
+        RedisSerializer<String> stringSerializer = new StringRedisSerializer();
+        // 解决key的\xac\xed\x00\x05t\x00\x01问题
+        redisTemplate.setKeySerializer(stringSerializer);
+        redisTemplate.setHashKeySerializer(stringSerializer);
 
+        // 解决value乱码问题
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        ObjectMapper om = new ObjectMapper();
+        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        jackson2JsonRedisSerializer.setObjectMapper(om);
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+
+        return redisTemplate;
+    }
 
 }
--- SpringBootDemo/src/test/java/com/cave/springbootdemo/example5/test/CachaApplicationTests.java
+++ SpringBootDemo/src/test/java/com/cave/springbootdemo/example5/test/CachaApplicationTests.java
@@ -1,25 +1,55 @@
 package com.cave.springbootdemo.example5.test;
 
 import com.cave.springbootdemo.example5.CacheApplication;
+import com.cave.springbootdemo.example5.pojo.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest( classes = {CacheApplication.class}) // 通过classes指定运行哪个SpringBootApplication
 public class CachaApplicationTests {
 
-    @Autowired
+    @Resource( name = "getRedisTemplate" ) // Autowired注解会重新生成一个对象，因此需要使用Resource
     private RedisTemplate redisTemplate;
 
     // RedisTemplate相关操作
     // https://blog.csdn.net/fengyao1995/article/details/72794899
+    // 需要有超时时间设置
 
+    /**
+     * 操作Redis String类型数据
+     *
+     * 如果是操作String类型的，最好使用StringRedisTemplate
+     */
     @Test
-    public void add(){
-        redisTemplate.opsForValue().set("1", "2");
+    public void valueOperations(){
+        // 获取ValueOperations操作对象
+        ValueOperations valueOperations = redisTemplate.opsForValue();
+        // 普通set方法
+        valueOperations.set("21231", "2");
+        // 设置set方法超时时间，TimeUnit是时间单位，时分秒等等
+//        valueOperations.set("2", "3", 10, TimeUnit.SECONDS); // 超时时间为10秒
+        // 删除
+//        redisTemplate.delete("1");
     }
+
+    /**
+     * Key = String, Value = Object
+     *
+     */
+    @Test
+    public void valueOperationsObject(){
+        ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+        User user = new User();
+        user.setUid(1);
+        valueOperations.set("1", user);
+        System.out.println(valueOperations.get("1"));
+    }
+
 }
